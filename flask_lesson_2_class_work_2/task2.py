e6452082a06cb079640c21698a259ca78c2b1935
Shchegolev <shegolevva2009@gmail.com>
2025-03-15 14:46:10 +0300

    flask_lesson_2_classwork_2

---
+++ flask_lesson_2_class_work_2/task2.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/<name>")
+@app.route("/index/<name>")
+def main(name):
+    return render_template("base.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080)
