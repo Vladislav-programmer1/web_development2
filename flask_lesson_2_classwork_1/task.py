7f5fe86274deb5976e6a404a58462e4e0170a981
Shchegolev <shegolevva2009@gmail.com>
2025-03-16 15:59:49 +0300

    sometimes... we need rest...

--- flask_lesson_2_classwork_1/task.py
+++ flask_lesson_2_classwork_1/task.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/<title>")
 @app.route("/index/<title>")
 def main(title):
-    return render_template("base.html", title=title)
+    return render_template("profession.html", title=title)
 
 
 if __name__ == "__main__":
