f0ac5db72b54e82e9476ddb57dac8269f8729601
Shchegolev <shegolevva2009@gmail.com>
2025-03-16 16:07:08 +0300

    flask_lesson_2_classwork_3

--- flask_lesson_2_class_work_3/task3.py
+++ flask_lesson_2_class_work_3/task3.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/<list_>")
 @app.route("/index/<list_>")
 def main(list_):
-    return render_template("profession.html", list=list_)
+    return render_template("task3.html", list=list_)
 
 
 if __name__ == "__main__":
