f26623c202fce55218eb81e86af3af081f8677a9
Shchegolev <shegolevva2009@gmail.com>
2025-03-26 11:26:25 +0300

    flask_lesson_3_classwork_3

--- flask_lesson_3/main.py
+++ flask_lesson_3/main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 from data import db_session
 from data.users import User
-from data.news import News
-from forms.user import RegisterForm
-
-import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -12,39 +8,65 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route("/")
 def index():
-    db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+    return render_template("index.html")
 
 
 def main():
-    db_session.global_init("db/blogs.db")
-    app.run(host="127.0.0.1", port=8080)
+    db_session.global_init("db/mars.db")
+    session = db_session.create_session()
+
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    session.add(user)
+
+    user = User()
+    user.surname = "Smith"
+    user.name = "Alex"
+    user.age = 41
+    user.position = "not captain"
+    user.speciality = "builder"
+    user.address = "module_2"
+    user.email = "smith@mars.org"
+    session.add(user)
+
+    user = User()
+    user.surname = "Uorhol"
+    user.name = "Ende"
+    user.age = 35
+    user.position = "not captain"
+    user.speciality = "drawer"
+    user.address = "module_3"
+    user.email = "uorhol@mars.org"
+    session.add(user)
+
+    user = User()
+    user.surname = "Boucher"
+    user.name = "Rid"
+    user.age = 29
+    user.position = "not captain"
+    user.speciality = "doctor"
+    user.address = "module_4"
+    user.email = "boucher@mars.org"
+    session.add(user)
+
+    user = User()
+    user.surname = "Komtua"
+    user.name = "Jhon"
+    user.age = 36
+    user.position = "not captain"
+    user.speciality = "biologist"
+    user.address = "module_5"
+    user.email = "komtua@mars.org"
+    session.add(user)
+    session.commit()
+
+    # app.run(host="127.0.0.1", port=8080)
 
 
 if __name__ == '__main__':
